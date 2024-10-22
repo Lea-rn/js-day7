ea8f3b8686815dd34028c3709d74d2baffaa0502
achrefmechergui <achreffcb123achref@gamil.com>
2024-10-22 22:19:01 +0100

    done

---
+++ app.js
@@ -0,0 +1,77 @@
+///// array (data structure) ::: 
+/// arrays are 0 based .
+
+// const friend1 = "eya" ; 
+// const friend2 = "karim" ;
+//  const friend3 = "mohamed" ;
+
+// const freinds = ["eya" , "karim" ,"mohamed", "eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed","eya" , "karim" ,"mohamed", ] 
+
+// console.log(freinds)
+// console.log(freinds[0])
+
+// const toul = freinds.length
+
+// console.log(toul)
+
+/// array[index]
+
+// console.log(freinds[freinds.length-1])
+
+
+
+// const years = new Array (1991,200,2003)
+// console.log(years[1])
+
+
+
+// const freinds = ["eya" , "karim" ,"mohamed"]
+// console.log(freinds)
+
+// freinds[2] = "salma" ;
+
+// freinds[0] = "yomna"
+
+// console.log(freinds)
+// const firstName = "nessrine"
+// function hello (){
+//    return `hello` 
+// }
+
+// const infomation = [firstName , "lastname" , 2000 , freinds , hello() ]
+
+// console.log(infomation)
+// console.log(infomation[3])
+
+
+
+///// arrays methods (add)  push , unshift ::: 
+
+///// .length ===> nchoufou beha etoul mte3 l'array ... 
+//// .push ====> tzid menteli 
+///// .unshift ===> tzid men kodem
+
+// const freinds = ["eya" , "karim" ,"mohamed"]
+
+
+// freinds.push("marwa")
+// freinds.unshift("nessrine")
+
+// // console.log(freinds[freinds.length-1])
+// console.log(freinds)
+
+///// arrays methods (delete)  pop , shift ::: 
+
+/// .pop tnakess menteli 
+/// .shift tnakess men kodem
+
+// const freinds = ["eya" , "karim" ,"mohamed","salma" , "yomna" ,"nessrine"]
+// console.log(freinds)
+// freinds.pop()
+// console.log(freinds)
+// freinds.shift()
+// console.log(freinds)
+
+// console.log
+
+///// .length , .push("hfhf") , .unshift("hfhfhf") , .pop() , .shif() ...
