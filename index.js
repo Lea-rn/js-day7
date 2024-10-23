e6952e8598c448f59241ab4e1c66887dcc4b92c8
achrefmechergui <achreffcb123achref@gamil.com>
2024-10-23 17:12:02 +0100

    updated

---
+++ index.js
@@ -0,0 +1,43 @@
+// const freinds = ["eya" , "karim" ,"mohamed"]
+
+// console.log(freinds[1])
+
+// freinds[1] = "achref" ; 
+// freinds[2] = "yahya"
+// console.log(freinds)
+
+// function log (){
+//  return "hello"
+// }
+// // console.log(log())
+// const lastname = "shmedtman"
+
+
+// const info = ["jhon",1990, log() , lastname , freinds ]
+
+// console.log(info[info.length-1])
+
+
+///// add element for the array :: /// push(tzid mel lekher) , unshif(tzid meloul)
+
+const freinds = ["eya" , "karim" ,"mohamed","jhon","peter"]
+
+console.log(freinds)
+
+freinds.push("elyes")
+
+console.log(freinds)
+
+freinds.unshift("ali")
+console.log(freinds)
+
+
+//// delete element for the array :: pop(tfassekh lekher) , shift(tfasakh men kodem)
+
+freinds.pop()
+console.log(freinds)
+freinds.shift()
+console.log(freinds)
+
+
+///// .length , .push , .unshift , .pop , .shift 
